a4a549437b70bfeee6545ad9f36793261320c4d7
DreamFXX <filipek.husak@gmail.com>
2025-01-07 23:36:09 +0100

    namespaces fix + Controller of MenuList created

---
+++ FoodMenu/FoodMenu/Controllers/FoodMenu.cs
@@ -0,0 +1,23 @@
+﻿using FoodMenu.Data;
+using FoodMenu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodMenu.Controllers
+{
+    public class FoodMenu : Controller
+    {
+        // Add a constructor to inject the FoodMenuContext
+        private readonly FoodMenuContext _context;
+        public FoodMenu(FoodMenuContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //await počká na odpověd contextu. async a await se používají pro asynchronní programování (Příprava snídaně).
+            return View(await _context.Dishes.ToListAsync());
+        }
+    }
+}
